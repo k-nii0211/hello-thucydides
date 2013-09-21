bd09ec1115eaddc3362d43922dd26f6f5c7c3cac
knii0211 <cm10030@gmail.com>
2013-09-21 21:49:25 +0900

    init commit

---
+++ src/test/java/org/knii/sample/hello_thucydides/requirements/Application.java
@@ -0,0 +1,11 @@
+package org.knii.sample.hello_thucydides.requirements;
+
+import net.thucydides.core.annotations.Feature;
+
+public class Application {
+    @Feature
+    public class Search {
+        public class SearchByKeyword {}
+        public class SearchByMultipleKeywords {}
+    }
+}
